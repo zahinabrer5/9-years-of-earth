e28e1a84e4057a8a3c174f1ef0bee3c4a42b9f32
Zahin Abrer <zahinabrer5@gmail.com>
2024-08-16 07:08:29 -0400

    Use proper way to save files in Python

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import random
 import requests
 import string
+import sys
 from dotenv import load_dotenv
 
 # https://stackoverflow.com/a/2257449
@@ -21,7 +22,7 @@ while start_date < end_date:
     r = requests.get(url, headers={'User-agent': rand_string(8)})
     i = 0
     while r.status_code != 200 and i < 10:
-        r = requests.get(url, headers={'User-agent': rand_string(8)})
+        r = requests.get(url, headers={'User-agent': rand_string(16)})
         i += 1
     if i == 10:
         start_date += datetime.timedelta(days=1)
@@ -35,10 +36,10 @@ while start_date < end_date:
 
         s = filename[8:] # strip off 'epic_1b_' from start of string
         fmt_datetime = '-'.join([s[:4], s[4:6], s[6:8]]) + '_' + ':'.join([s[8:10], s[10:12], s[12:14]])
-        file_to_save = f'orig/{fmt_datetime}.png'
+        save_path = os.path.join(sys.path[0], fmt_datetime+'.png')
 
-        # save image_url as file_to_save
-        open(file_to_save, 'wb').write(requests.get(image_url).content)
+        # save image_url to save_path
+        open(save_path, 'wb').write(requests.get(image_url).content)
 
         # make copy in frames/ & edit with Pillow (add fmt_datetime to bottom centre of image)
         text = fmt_datetime.replace('_', ' ')
